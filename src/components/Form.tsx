e5f56c541e6c46ff0d7e61cf8868abb2d2314a48
1129-tame <onemanlive0603tame@yahoo.ne.jp>
2022-02-18 08:26:01 +0000

    wiki api 取得

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,27 +1,36 @@
-import { VFC } from 'react';
+import { VFC, useEffect } from 'react';
 import { Grid, TextField, Button } from '@material-ui/core';
+import axios from 'axios';
 
-const Form: VFC = () => (
-  <Grid
-    container
-    spacing={3}
-    direction="row"
-    justifyContent="center"
-    alignItems="center"
-  >
-    <Grid item xs={12}>
-      <form noValidate autoComplete="off">
-        <TextField id="standard-basic" label="name" fullWidth />
-        {/* <TextField id="filled-basic" label="Filled" variant="filled" />
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" /> */}
-      </form>
+const Form: VFC = () => {
+  // ランダムに wikipedea の情報を取得
+  useEffect(() => {
+    axios.get('https://ja.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&grnlimit=2&prop=info&format=json&origin=*').then((res) => {
+      // const a = res;
+      console.log(res);
+    });
+  });
+
+  return (
+    <Grid
+      container
+      spacing={3}
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Grid item xs={12}>
+        <form noValidate autoComplete="off">
+          <TextField id="standard-basic" label="name" fullWidth />
+        </form>
+      </Grid>
+      <Grid>
+        <Button variant="contained" color="primary" size="large">
+          Push!
+        </Button>
+      </Grid>
     </Grid>
-    <Grid>
-      <Button variant="contained" color="primary" size="large">
-        Push!
-      </Button>
-    </Grid>
-  </Grid>
-);
+  );
+};
 
 export default Form;
